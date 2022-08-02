5f2d321a148182bc534d27dbc1025080e039ac0a
Juan Freitas <70648045+JuanFreitas094@users.noreply.github.com>
2022-08-02 01:01:30 -0300

    Adicionado div de favoritos

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,37 +1,37 @@
-import React, { ReactNode, useState } from "react";
+import React, { Children, ReactNode, useState } from "react";
 import styles from "./Card.module.scss";
 import { AiOutlineHeart } from 'react-icons/ai'
 import { VscChromeClose } from 'react-icons/vsc'
 import { FiEdit } from 'react-icons/fi'
 import { IVehicle } from "../../types/Vehicle";
+import { AiFillHeart } from 'react-icons/ai'
 
 interface ICard {
   title: string;
   children: ReactNode;
+  vehicle: IVehicle;
+  onClick: (vehicle: IVehicle) => void;
 }
 
-const Card = (props: ICard) => {
+const Card = (props: ICard) => {  
+  const[isFavorited, setFavorite] = useState<boolean>(props.vehicle.isFavorite);
+
+  const handleClick = () => {
+    props.onClick(props.vehicle);
+  }
+
   return (
     <div className={styles.Card}>
       <div className={styles.Icons}>
-        <div><AiOutlineHeart /></div>
-        <div><VscChromeClose /></div>
-        <div><FiEdit /></div>
+        <button onClick={handleClick}>
+          { isFavorited ? <AiFillHeart/> : <AiOutlineHeart/> }
+        </button>
+        <button><VscChromeClose /></button>
+        <button><FiEdit /></button>
       </div>
       <h2>{props.title}</h2>
       <div className={styles.content}>{props.children}</div>
     </div>
   );
-    const VerifyFav = () => {
-      const [vehicles, setVehicles] = useState<IVehicle[]>([]);
-      const [favorite, setFavorite] = useState<boolean>(false);
-
-      const handleVerifyFav = () => {
-        // setFavorite(!isFavorite);
-      }
-        <div className={styles.Favorite}>
-          <button className={styles.Filter} onClick={handleVerifyFav}><AiOutlineHeart/></button>
-        </div>
-    };
-  };
+};
 export default Card;
--- src/pages/Vehicles/index.tsx
+++ src/pages/Vehicles/index.tsx
@@ -7,13 +7,39 @@ import AddPage from "../Add";
 import { TbAdjustmentsHorizontal } from 'react-icons/tb';
 import FilterPage from "../../pages/Filter";
 
+var vehicles1: IVehicle[] = new Array;
+var teste: IVehicle = {
+  id: 1,
+  plate: "GFM0495",
+  isFavorite: false,
+  createdAt: new Date(2022, 10, 2, 2, 20, 1, 10),
+  name: "TESTE1",
+  price: 100,
+  description: "OLA",
+  year: 2300,
+  color: "VERDÃO" 
+}
+vehicles1.push(teste);
+
+var teste: IVehicle = {
+  id: 2,
+  plate: "GFM0495",
+  isFavorite: true,
+  createdAt: new Date(2022, 10, 2, 2, 20, 1, 10),
+  name: "TESTE2",
+  price: 100,
+  description: "OLA",
+  year: 2300,
+  color: "VERDÃO" 
+}
+vehicles1.push(teste);
+
 const VehiclesPage = () => {
-  const [vehicles, setVehicles] = useState<IVehicle[]>([]);
+  const [vehicles, setVehicles] = useState<IVehicle[]>(vehicles1);
   const [search, setSearch] = useState<string>("");
 
   const[isFilterOpened, setFilterOpened] = useState<boolean>(false);
   const[isAddOpened, setAddOpened] = useState<boolean>(false);
-
 
   const handleFilterClick = () => {
     setFilterOpened(!isFilterOpened);
@@ -22,17 +48,16 @@ const VehiclesPage = () => {
   const handleAddClick = () => {
     setAddOpened(!isAddOpened);
   }
-
-  var teste: IVehicle = {
-    id: 1,
-    plate: "GFM0495",
-    isFavorite: false,
-    createdAt: new Date(2022, 10, 2, 2, 20, 1, 10),
-    name: "TESTE",
-    price: 100,
-    description: "OLA",
-    year: 2300,
-    color: "VERDÃO" 
+  
+  const handleFavoriteClick = (vehicle: IVehicle)  => {
+    let vehicleAux = [...vehicles];
+    for (let i = 0; i<= vehicleAux.length; i++) {
+      if (vehicleAux[i].id === vehicle.id) {
+        vehicleAux[i].isFavorite = !vehicle.isFavorite;
+        break;
+      }
+    }
+    setVehicles(vehicleAux);
   }
 
   useEffect(() => {
@@ -55,41 +80,56 @@ const VehiclesPage = () => {
               <Search placeholder="Buscar" value={search} onChange={() => {}} />
               <button className={styles.Filter} onClick={handleFilterClick}><TbAdjustmentsHorizontal/></button>
             </div>
-              <Button text="ADICIONAR" onClick={handleAddClick} />   
-              <h2>Meus Anúncios</h2>
-              <div className={styles.Cards}>
-            <Card
-              title={teste.name}
-              children={
-                <>
-                  <p>{teste.price}</p>
-                  <p>{teste.description}</p>
-                  <p>{teste.year}</p>
-                  <p>{teste.color}</p>
-                </>              
-              }
-              />   
+              <Button text="ADICIONAR" onClick={handleAddClick} /> 
 
-            {/* {
-              vehicles.map((value,pos) =>
-                <Card
-                  title={value.name}
-                  children={
-                    [
-                      value.price,
-                      value.description,
-                      value.year,
-                      value.color
-                    ]
-                  }
-                />          
-              )
-            } */}
-            <Card title="Sandero Stepway">
-              <p>Price: 22000</p>
-              <p>Description: Carro usado por 2 anos...</p>
-              <p>Year: 2018</p>
-            </Card>
+              <h2>Favoritos</h2>
+              <div className={styles.Cards}>
+                {
+                  vehicles.filter(function(obj) {
+                    if (obj.isFavorite) {
+                      return obj;
+                    } 
+                  }).map((value, pos) =>
+                  <Card
+                      title={value.name}
+                      children={
+                        <>
+                          <p>{teste.price}</p>
+                          <p>{teste.description}</p>
+                          <p>{teste.year}</p>
+                          <p>{teste.color}</p>
+                        </>    
+                      }
+                      vehicle={value}
+                      onClick={handleFavoriteClick}
+                    />
+                  )
+                }
+              </div>
+              
+              <h2>Meus Anúncios</h2>
+              <div className={styles.Cards}>           
+                {
+                  vehicles.filter(function(obj) {
+                    if (!obj.isFavorite) {
+                      return obj;
+                    }
+                  }).map((value, pos) =>
+                  <Card
+                      title={value.name}
+                      children={
+                        <>
+                          <p>{teste.price}</p>
+                          <p>{teste.description}</p>
+                          <p>{teste.year}</p>
+                          <p>{teste.color}</p>
+                        </>    
+                      }
+                      vehicle={value}
+                      onClick={handleFavoriteClick}
+                    />
+                  )
+              }
             </div>          
           </>
           : null          
@@ -107,4 +147,3 @@ const VehiclesPage = () => {
 };
 
 export default VehiclesPage;
-
